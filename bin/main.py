0e0947ce550dc656421b12076159cb392cf1b760
ikeananaba@gmail.com <ikeananaba@gmail.com>
2023-07-31 10:55:10 +0100

    Added virtual realtime reading

--- bin/main.py
+++ bin/main.py
@@ -28,7 +28,6 @@ class LicensePlateRecognition:
         self.tracker = Sort()
         self.license_plate_detector = YOLO('./models/license_plate_detector.pt')
         self.car_detector = YOLO('yolov8s.pt')
-        self.car_identifier = CarIdentifier()
 
         self.vid_stream = cv2.VideoCapture('./sample.mp4')
         # self.vid_stream = cv2.VideoCapture(0)
@@ -39,13 +38,18 @@ class LicensePlateRecognition:
         self.latest_frame = None
 
         self.plate_image = None
+        self.plate_images = []
+        self.image_size_up = 50
+        self.image_scale = 2
 
         self.running = True
 
         self.video_stream_thread = Thread(target=self.video_stream)
         self.car_detection_thread = Thread(target=self.car_recognition)
+        # self.license_plate_extractor_thread = Thread(target=self.license_plate_extractor)
         self.video_stream_thread.start()
         self.car_detection_thread.start()
+        # self.license_plate_extractor_thread.start()
 
     def video_stream(self):
         while self.running:
@@ -58,8 +62,9 @@ class LicensePlateRecognition:
 
             for detection in self.detections:
                 x1, y1, x2, y2, id = detection
-                x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
+                # x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+                #
                 # frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 4)
                 # frame = cv2.putText(
                 #     frame,
@@ -71,7 +76,7 @@ class LicensePlateRecognition:
                 #     2,
                 # )
 
-            frame = cv2.resize(frame, (1280, 720))
+            frame = cv2.resize(frame, (640, 360))
             cv2.imshow(self.main_frame, frame)
             if self.plate_image is not None:
                 cv2.imshow("temp Frame", self.plate_image)
@@ -87,26 +92,47 @@ class LicensePlateRecognition:
         while self.running:
             if self.latest_frame is None:
                 continue
-            detections = self.car_detector(self.latest_frame, verbose=False)[0]
+            detections = self.license_plate_detector(self.latest_frame, verbose=False)[0]
 
             frame = self.latest_frame
             self.detections = []
-            # boxes = detections[0].boxes.xyxy.cpu().numpy().astype(int)
-            # ids = detections[0].boxes.id.cpu().numpy().astype(int)
 
             for detection in detections.boxes.data.tolist():
                 x1, y1, x2, y2, score, class_id = detection
 
-                if score >= 0.8:
-                    plate_image = frame[int(y1):int(y2), int(x1):int(x2)]
+                self.detections.append([x1, y1, x2, y2, class_id])
+                try:
+                    plate_image = frame[int(y1 - self.image_size_up):int(y2 + self.image_size_up),
+                                        int(x1 - self.image_size_up):int(x2 + self.image_size_up)]
+                except:
+                    plate_image = frame[int(y1 - self.image_size_up/2):int(y2 + self.image_size_up/2),
+                                        int(x1 - self.image_size_up/2):int(x2 + self.image_size_up/2)]
 
-                    plat_det = self.license_plate_detector(plate_image, verbose=False)[0]
-                    if len(plat_det.boxes) > 0:
-                        self.plate_image = plate_image
+                # scale license plate
+                # plate_image = cv2.resize(plate_image, (int(plate_image.shape[1] * self.image_scale),
+                #                                        int(plate_image.shape[0] * self.image_scale)))
+                # process license plate
+                plate_img = cv2.cvtColor(plate_image, cv2.COLOR_BGR2GRAY)
+                _, plate_img = cv2.threshold(plate_img, 64, 255, cv2.THRESH_BINARY_INV)
 
-            time.sleep(2)
-                # if int(class_id) in self.eligible_vehicles:
-                # self.detections.append([x1, y1, x2, y2, class_id])
+                # read license plate number
+                license_plate_text, license_plate_text_score = read_license_plate(plate_img)
+
+                if license_plate_text is not None:
+                    print(license_plate_text)
+
+    def license_plate_extractor(self):
+        while self.running:
+            for image in self.plate_images:
+                # process license plate
+                plate_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                _, plate_img = cv2.threshold(plate_img, 64, 255, cv2.THRESH_BINARY_INV)
+
+                # read license plate number
+                license_plate_text, license_plate_text_score = read_license_plate(plate_img)
+
+                print(license_plate_text)
+                self.plate_images.remove(image)
 
 
 if __name__ == '__main__':
